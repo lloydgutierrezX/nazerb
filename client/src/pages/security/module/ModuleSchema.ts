b1909a0ebcd62cea220f5201789ae4dc944f326b
lloyd <lloydgutierrez24@gmail.com>
2025-05-21 18:02:17 +0800

    module - pushed unpushed code

--- client/src/pages/security/module/ModuleSchema.ts
+++ client/src/pages/security/module/ModuleSchema.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const moduleSchema = z.object({
   name: z.string().min(4).max(60).default(""),
+  link: z.string().min(1).max(120),
   description: z.string().nullable().default(null),
   active: z.boolean().default(true),
   createdAt: z.date().nullish(),
