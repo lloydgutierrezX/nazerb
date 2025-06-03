2445db4bf06d73a1bd20d2ce14e78b0a362517cc
sean <seannazerb@gmail.com>
2025-06-03 20:45:24 +0800

    IMenu inital test1

--- client/src/components/menu/IMenu.tsx
+++ client/src/components/menu/IMenu.tsx
@@ -52,6 +52,12 @@ export const jsonMenu = [
         is_active: true,
         icon: "file-user",
       },
+      {
+        key: "employee-type",
+        path: "management/employee-type",
+        is_active: true,
+        icon: "file-user",
+      },
     ],
   },
   {
