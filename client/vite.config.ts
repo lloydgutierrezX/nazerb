ad159375c60204b63f952b40b34db05fa13a69b3
lloyd <lloydgutierrez24@gmail.com>
2025-06-04 15:12:14 +0800

    master - reset previous changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-// import dotenv from 'dotenv';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
-  define: {
-    'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL || 'http://localhost'),
-    'process.env.VITE_PROJECT_TITLE': JSON.stringify(process.env.VITE_PROJECT_TITLE || 'NAZERB - ESS'),
-    'process.env.VITE_REQUEST_TIMEOUT': JSON.stringify(process.env.VITE_REQUEST_TIMEOUT || 2000),
-  },
+  server: {
+    host: true,
+    port: 80,
+  }
 })
