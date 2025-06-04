e11d961a4db1171ec74385e08af8fe03334c5a5c
lloyd <lloydgutierrez24@gmail.com>
2025-06-04 14:13:46 +0800

    master - viteconfig test

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+// import dotenv from 'dotenv';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
-  server: {
-    host: true,
-    port: 80,
-  }
+  define: {
+    'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL || 'http://localhost'),
+    'process.env.VITE_PROJECT_TITLE': JSON.stringify(process.env.VITE_PROJECT_TITLE || 'NAZERB - ESS'),
+    'process.env.VITE_REQUEST_TIMEOUT': JSON.stringify(process.env.VITE_REQUEST_TIMEOUT || 2000),
+  },
 })
