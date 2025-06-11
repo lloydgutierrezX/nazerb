267225db744c337eccbda12b0a0bbc188e24fd1b
Driyana <ricci.nazerb@gmail.com>
2025-06-11 17:22:11 +0800

    client-benefits - initial push

--- client/src/pages/management/position/PositionSchema.ts
+++ client/src/pages/management/position/PositionSchema.ts
@@ -1,10 +1,6 @@
 
 import { z } from "zod";
-<<<<<<< HEAD
 export const PositionSchema = z.object({
-=======
-export const employeeStatusSchema = z.object({
->>>>>>> fe75a4b711106bcdaeaf02053435b0e87bb7f89b
   active: z.coerce.boolean(),
   name: z.string().min(4).max(60),
   description: z.string().max(255).nullable(),
@@ -12,8 +8,4 @@ export const employeeStatusSchema = z.object({
   updatedAt: z.date().nullish(),
 });
 
-<<<<<<< HEAD
 export type IPosition = z.infer<typeof PositionSchema>;
-=======
-export type IPosition = z.infer<typeof employeeStatusSchema>;
->>>>>>> fe75a4b711106bcdaeaf02053435b0e87bb7f89b
